f34a29af4c78d56c84a762aee57679cebed426b9
Mahmoud elqenawey <mahmoudelqenawey1@gmail.com>
2025-03-07 22:47:36 +0200

    update

--- src/pages/ClassDetailsPage.tsx
+++ src/pages/ClassDetailsPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
 import {
   Box,
   Heading,
@@ -82,7 +82,7 @@ interface SessionForm {
 }
 
 export default function ClassDetailsPage() {
-  const { classId } = useParams();
+  //   const { classId } = useParams();
   const navigate = useNavigate();
   const { open, onOpen, onClose } = useDisclosure();
   const [activeTab, setActiveTab] = useState<
